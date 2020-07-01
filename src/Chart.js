deab4412df3947da306c19ee78a5daedcd07acd1
r2abreu <r2abreu@protonmail.com>
2020-07-01 22:12:54 +0200

    apply react components

---
+++ src/Chart.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Chart = () => {
+	return (
+		<div class="chart-wrapper">
+			<canvas id="chart" height="400" width="400" />
+		</div>
+	);
+};
+
+export default Chart;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,64 @@
+// Import the React and ReactDOM libraries
+
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import axios from 'axios';
+import DataBlock from './DataBlock';
+import ApiMetadata from './ApiMetadata';
+import Chart from './Chart';
+import './styles.css';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+// Create a react component
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+// This is a Functional component
+
+class App extends React.Component {
+	state = { data: {}, meta: {} };
+
+	async componentDidMount() {
+		const dataResponse = await axios.get('https://api.covid19api.com/summary');
+		this.setState({ data: dataResponse.data.Global });
+		console.log('data:', this.state.data);
+
+		const metaResponse = await axios.get('https://api.covid19api.com/stats');
+		this.setState({ meta: metaResponse.data });
+		console.log('meta:', this.state.meta);
+	}
+
+	render() {
+		return (
+			<div>
+				<div className="api-metadata">
+					<ApiMetadata update={this.state.meta} />
+				</div>
+				<div className="data-block">
+					<div className="cases data-container">
+						<div>
+							<h2>Cases</h2>
+							<DataBlock new={this.state.data.NewConfirmed} total={this.state.data.TotalConfirmed} />
+						</div>
+					</div>
+					<div className="deaths data-container">
+						<div>
+							<h2>Deaths</h2>
+							<DataBlock new={this.state.data.NewDeaths} total={this.state.data.TotalDeaths} />
+						</div>
+					</div>
+					<div className="recoveries data-container">
+						<div>
+							<h2>Recoveries</h2>
+							<DataBlock new={this.state.data.NewRecovered} total={this.state.data.TotalRecovered} />
+						</div>
+					</div>
+				</div>
+				<div className="chart">
+					<Chart />
+				</div>
+			</div>
+		);
+	}
+}
+
+// Take the react component and show it on the screen
+
+ReactDOM.render(<App />, document.getElementById('root'));
